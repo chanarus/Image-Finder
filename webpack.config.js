8c68504de2b0074ac371ce7014c25571ffe60a3e
The Game <chanarusampath@gmail.com>
2018-04-28 15:45:03 +0530

    Added webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+var htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, use: 'babel-loader' }
+        ]
+    },
+    plugins: [new htmlWebpackPlugin({
+        template: 'src/index.html'
+    })]
+}
